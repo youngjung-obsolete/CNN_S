d5e4d8e03301bff905488bdde8e35cff94092387
youngjung <crazyyfish@naver.com>
2017-02-13 22:58:48 +0900

    bn

--- CNN_S.py
+++ CNN_S.py
@@ -50,7 +50,7 @@ import flags
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.9999		 # The decay to use for the moving average.
-NUM_EPOCHS_PER_DECAY = 350.0			# Epochs after which learning rate decays.
+NUM_EPOCHS_PER_DECAY = 1.0			# Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 0.1	# Learning rate decay factor.
 INITIAL_LEARNING_RATE = 0.1			 # Initial learning rate.
 
@@ -121,32 +121,7 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
 		tf.add_to_collection('losses', weight_decay)
 	return var
 
-def inputs(eval_data):
-	"""Construct input for CIFAR evaluation using the Reader ops.
-
-	Args:
-		eval_data: bool, indicating if one should use the train or eval data set.
-
-	Returns:
-		images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
-		labels: Labels. 1D tensor of [batch_size] size.
-
-	Raises:
-		ValueError: If no data_dir
-	"""
-	if not FLAGS.data_dir:
-		raise ValueError('Please supply a data_dir')
-	data_dir = os.path.join(FLAGS.data_dir, 'cifar-10-batches-bin')
-	images, labels = cifar10_input.inputs(eval_data=eval_data,
-																				data_dir=data_dir,
-																				batch_size=FLAGS.batch_size)
-	if FLAGS.use_fp16:
-		images = tf.cast(images, tf.float16)
-		labels = tf.cast(labels, tf.float16)
-	return images, labels
-
-
-def inference( images, num_classes ):
+def inference( images, num_classesi, phase_train ):
 	"""Build the CNN_S model.
 
 	Args:
@@ -158,14 +133,19 @@ def inference( images, num_classes ):
 	end_points = {}
 	with tf.variable_scope('CNN_S') as sc:
 		end_points['conv1'] = slim.conv2d( images, 96, [7, 7], stride=2, padding='VALID', scope='conv1')
-		end_points['lrn'] = tf.nn.local_response_normalization( end_points['conv1'] )
-		end_points['pool1'] = slim.max_pool2d(end_points['lrn'], [3, 3], stride=3, scope='pool1')
+		#end_points['lrn'] = tf.nn.local_response_normalization( end_points['conv1'] )
+		end_points['conv1_bn'] = batch_norm( end_points['conv1', 96, phase_train )
+		end_points['pool1'] = slim.max_pool2d(end_points['conv1_bn'], [3, 3], stride=3, scope='pool1')
 		end_points['conv2'] = slim.conv2d( end_points['pool1'], 256, [5, 5], stride=1, padding='SAME', scope='conv2')
-		end_points['pool2'] = slim.max_pool2d(end_points['conv2'], [2, 2], stride=2, scope='pool2')
-		end_points['conv3'] = slim.conv2d( end_points['pool2'], 512, [5, 5], stride=1, padding='SAME', scope='conv3')
-		end_points['conv4'] = slim.conv2d( end_points['conv3'], 512, [5, 5], stride=1, padding='SAME', scope='conv4')
-		end_points['conv5'] = slim.conv2d( end_points['conv4'], 512, [5, 5], stride=1, padding='SAME', scope='conv5')
-		end_points['pool5'] = slim.max_pool2d(end_points['conv5'], [3, 3], stride=3, scope='pool5')
+		end_points['conv2_bn'] = batch_norm( end_points['conv2', 256, phase_train )
+		end_points['pool2'] = slim.max_pool2d(end_points['conv2_bn'], [2, 2], stride=2, scope='pool2')
+		end_points['conv3'] = slim.conv2d( end_points['pool2'], 512, [3, 3], stride=1, padding='SAME', scope='conv3')
+		end_points['conv3_bn'] = batch_norm( end_points['conv3', 512, phase_train )
+		end_points['conv4'] = slim.conv2d( end_points['conv3_bn'], 512, [3, 3], stride=1, padding='SAME', scope='conv4')
+		end_points['conv4_bn'] = batch_norm( end_points['conv4', 512, phase_train )
+		end_points['conv5'] = slim.conv2d( end_points['conv4'], 512, [3, 3], stride=1, padding='SAME', scope='conv5')
+		end_points['conv5_bn'] = batch_norm( end_points['conv5', 512, phase_train )
+		end_points['pool5'] = slim.max_pool2d(end_points['conv5_bn'], [3, 3], stride=3, scope='pool5')
 		# Use conv2d instead of fully_connected layers.
 		end_points['fc6'] = slim.conv2d(end_points['pool5'], 4096, [6, 6], padding='VALID', scope='fc6')
 		end_points['dropout6'] = slim.dropout(end_points['fc6'], scope='dropout6')
---
+++ batch_norm.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+def batch_norm(x, n_out, phase_train):
+	"""
+	Batch normalization on convolutional maps.
+	Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+	Args:
+		x:		   Tensor, 4D BHWD input maps
+		n_out:	   integer, depth of input maps
+		phase_train: boolean tf.Varialbe, true indicates training phase
+		scope:	   string, variable scope
+	Return:
+		normed:	  batch-normalized maps
+	"""
+	with tf.variable_scope('bn'):
+		beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+									 name='beta', trainable=True)
+		gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+									  name='gamma', trainable=True)
+		batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
+		ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+		def mean_var_with_update():
+			ema_apply_op = ema.apply([batch_mean, batch_var])
+			with tf.control_dependencies([ema_apply_op]):
+				return tf.identity(batch_mean), tf.identity(batch_var)
+
+		mean, var = tf.cond(phase_train,
+							mean_var_with_update,
+							lambda: (ema.average(batch_mean), ema.average(batch_var)))
+		normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+	return normed
+
